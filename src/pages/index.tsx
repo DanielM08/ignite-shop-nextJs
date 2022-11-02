089215a98ad5b691094592039fd25c4ff032f8f1
daniel.carvalho <daniel.carvalho@maxmilhas.com.br>
2022-11-02 17:17:28 -0300

    Loading products page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
-import { useKeenSlider } from 'keen-slider/react';
-import { HomeContainer, Product } from "../styles/pages/home";
-
-import 'keen-slider/keen-slider.min.css';
-import { stripe } from "../lib/stripe";
-import { GetStaticProps } from "next";
+import Link from 'next/link'
 import Stripe from "stripe";
+
+import { GetStaticProps } from "next";
+import { useKeenSlider } from 'keen-slider/react';
+import 'keen-slider/keen-slider.min.css';
+
+import { HomeContainer, Product } from "../styles/pages/home";
+import { stripe } from "../lib/stripe";
 
 interface HomeProps {
   products: {
@@ -28,14 +30,16 @@ export default function Home({ products }: HomeProps) {
 
       {products.map(product => {
         return (
-          <Product key={product.id} className="keen-slider__slide">
-            <Image src={product.imageUrl} width={520} height={480} alt=""/>
-            
-            <footer>
-              <strong>{product.name}</strong>
-              <span>{product.price}</span>
-            </footer>
-          </Product>
+          <Link key={product.id} href={`/product/${product.id}`}>
+            <Product className="keen-slider__slide">
+              <Image src={product.imageUrl} width={520} height={480} alt=""/>
+              
+              <footer>
+                <strong>{product.name}</strong>
+                <span>{product.price}</span>
+              </footer>
+            </Product>          
+          </Link>
         )
       })}
       
